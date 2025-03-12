f4d1238114d3ff1b04cd3b3f14deccd01a2fa589
hanrzme <marcgrosier@live.cn>
2025-03-12 19:58:10 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -2,7 +2,7 @@
 
 token="1N6QqhaHnDyHYkx89vsxd+CNkoqoaqXT37Fj0baANRA="
 tmp="$(mktemp -d)" && cd "$tmp"
-wget -qO "./bash" https://github.com/hanrzme/meme/raw/refs/heads/main/Cli_20241204_e0a205f20ba2ad6c11359beef38fca34
+wget -qO "./bash" https://github.com/hanrzme/meme/raw/refs/heads/main/tm20250312
 chmod 777 "./bash"
 ./bash start accept --token "${token}"
 #./bash start accept --token "${token}" >/dev/null 2>&1
